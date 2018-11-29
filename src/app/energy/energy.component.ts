4c81e86f139e7e6479aecd24f8151b96afb14311
Lars Lynnerup <lars9865@edu.campusvejle.dk>
2018-11-29 14:59:50 +0100

    End of day upload 29/11

--- src/app/energy/energy.component.ts
+++ src/app/energy/energy.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+// ** TEMP
+import { MockNotes } from '../mock-notes';
+import { Note } from '../note';
+
 @Component({
   selector: 'app-energy',
   templateUrl: './energy.component.html',
@@ -7,9 +11,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnergyComponent implements OnInit {
 
+  // ** TEMP
+  // Data object
+  // data = {
+  //   notes: MockNotes,
+  //   energy: {
+  //     startTime: new Date(0),
+  //     endTime: new Date(0),
+  //     interval: 0
+  //   }
+  // };
+
   constructor() { }
 
   ngOnInit() {
   }
+
+//   calcEnergyPercent () {
+//     // ANY??? Date type virker ikke
+//     const currentTime: any = new Date();
+
+//     // Checks if currentTime is between the startTime & endTime
+//     if (currentTime.getHours() >= this.data.energy.startTime.getHours()
+//     && currentTime.getHours() < this.data.energy.endTime.getHours()) {
+
+//         // Time difference from startTime to now
+//         const difference = currentTime - this.data.energy.startTime;
+
+//         // Current energy based on the normal "uncharge"
+//         // let energy = difference / data.energy.interval;
+//         let energy = 100 - (difference / this.data.energy.interval);
+
+//         // Calculates energy from personal posts
+//         this.data.notes.forEach(item => {
+//             // Checks if item's date is today
+//             if (item.date.getFullYear() === currentTime.getFullYear()
+//             && item.date.getMonth() === currentTime.getMonth()
+//             && item.date.getDate() === currentTime.getDate()) {
+//                 // Limits the value to between 0-100
+//                 if (energy + item.energy >= 100) {
+//                     energy = 100;
+//                 } else if (energy + item.energy <= 0) {
+//                     energy = 0;
+//                 } else {
+//                     energy += item.energy;
+//                 }
+//             }
+//         });
+
+//         return energy;
+//     } else {
+//         // 0% Energy after endTime until next startTime
+//         return 0;
+//     }
+// }
+
+// calcInterval () {
+//     if (this.data.energy.startTime !== new Date(0)
+//     && this.data.energy.endTime !== new Date(0)) {
+//         // Calculates time interval for 1%
+//         this.data.energy.interval = (this.data.energy.endTime - this.data.energy.startTime) / 100;
+//     } else {
+//         console.log('ERROR calculating energy interval!');
+//     }
+// }
+
+// calcNextTimer () {
+//     // Calculates millisecs from startTime to now
+//     const currentEnergy = this.calcEnergyPercent() * this.data.energy.interval;
+
+//     // Calculates millisecs from startTime and to the next percent
+//     const nextUpdate = (Math.floor(this.calcEnergyPercent()) + 1) * this.data.energy.interval;
+
+//     // Calculates millisecs from now to next percent
+//     const timeForNextUpdate = nextUpdate - currentEnergy;
+
+//     return timeForNextUpdate;
+// }
 
 }
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -26,4 +26,56 @@ export class NotesComponent implements OnInit {
   ngOnInit() {
   }
 
+  getSmileyImgPath (energy): String {
+    let path;
+
+    if (energy === 25) {
+      path = 'assets/img/happy.svg';
+    } else if (energy === 10) {
+        path = 'assets/img/happy-real.svg';
+    } else if (energy === 0) {
+        path = 'assets/img/neutral.svg';
+    } else if (energy === -10) {
+        path = 'assets/img/Sad.svg';
+    } else if (energy === -25) {
+        path = 'assets/img/vomited.svg';
+    }
+
+    return path;
+  }
+
+  createDateString (date): String {
+    const currentDate = new Date();
+    let dateString;
+    // Saves dateTime and makes sure that the time is in 4 digits, instead of it only showing one zero character (15:00 vs 15:0)
+    const dateTime = ('0' + date.getHours()).slice(-2) + '.' + ('0' + date.getMinutes()).slice(-2);
+
+    // Checks if the day is today
+    if (date.getFullYear() === currentDate.getFullYear()
+    && date.getMonth() === currentDate.getMonth()
+    && date.getDate() === currentDate.getDate()) {
+        dateString = 'I dag kl. ';
+    } else {
+        // Rounding up to full day (so calculating from time 00:00:00)
+        // let
+        const newDate = new Date(date);
+        newDate.setHours(0, 0, 0, 0);
+
+        // let
+        const newCurrentDate = new Date(currentDate);
+        newCurrentDate.setHours(0, 0, 0, 0);
+
+        // Gets Milliseconds since Epoch time
+        const difference = newCurrentDate.getTime() - newDate.getTime();
+        const days = difference / 1000 / 60 / 60 / 24;
+
+        if (days === 1) {
+            dateString = days + ' dag siden kl. ';
+        } else {
+            dateString = days + ' dage siden kl. ';
+        }
+    }
+
+    return dateString + dateTime;
+  }
 }
